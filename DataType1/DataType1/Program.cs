28c2e2aa38f51c1987ddaf4e78429c8fcfeaa53c
WebsterSweden <webster@webster.se>
2020-10-05 22:36:25 +0200

    Create and edit DataType1, Main1.

---
+++ DataType1/DataType1/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DataType1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Value types.
+
+            // Reference types.
+        }
+    }
+}
---
+++ Main1/Main1/Program.cs
@@ -0,0 +1,23 @@
+//using System;
+
+//namespace Main1
+//{
+//class Program
+//{
+//static void Main(string[] args)
+//{
+//Console.WriteLine("Hello World!");
+//}
+//}
+//}
+
+// CS5001 Program does not contain a static 'Main' method suitable for an entry point.
+// Must have Class.
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine(("Hello World"));
+    }
+}
