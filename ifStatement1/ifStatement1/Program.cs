c64d2ecffdcea37b3bde3caea5bcbca228cd926e
WebsterSweden <webster@webster.se>
2020-10-07 22:39:08 +0200

    Create and edit ifStatement1.

---
+++ ifStatement1/ifStatement1/Program.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace ifStatement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool boolCheck = true;
+            bool boolCheck = false;
+
+            //if (boolCheck == true)
+            //{
+            //    Console.WriteLine("boolCheck: " + boolCheck);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("boolCheck: " + boolCheck);
+            //}
+
+            // Unary ! (logical negation) operator.
+            //if (!true)
+            //{
+            //    Console.WriteLine("Unary operator: " + true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Unary operator: " + false);
+            //}
+
+            bool boolTrueOne = true;
+            bool boolTrueTwo = true;
+            bool boolFalseOne = false;
+            bool boolFalseTwo = false;
+
+            int intOne = 3;
+            int intTwo = 3;
+
+            if (boolTrueOne & boolTrueTwo)
+            {
+                Console.WriteLine("if boolTrueOne & boolTrueTwo: " + boolTrueOne + boolTrueTwo);
+            }
+            else
+            {
+                Console.WriteLine("else boolTrueOne & boolTrueTwo: " + boolTrueOne + boolTrueTwo);
+            }
+
+            if (boolTrueOne & boolFalseOne)
+            {
+                Console.WriteLine("if boolTrueOne & boolFalseOne: " + boolTrueOne + boolFalseOne);
+            }
+            else
+            {
+                Console.WriteLine("else boolTrueOne & boolFalseOne: " + boolTrueOne + boolFalseOne);
+            }
+
+            if (boolFalseOne & boolTrueOne)
+            {
+                Console.WriteLine("if boolFalseOne & boolTrueOne: " + boolFalseOne + boolTrueOne);
+            }
+            else
+            {
+                Console.WriteLine("else boolFalseOne & boolTrueOne: " + boolFalseOne + boolTrueOne);
+            }
+
+            //if (boolTrueOne && boolTrueTwo)
+            //{
+            //    //Console.WriteLine("boolTrueOne && boolTrueTwo: " + boolTrueOne + boolTrueTwo);
+            //}
+
+            //if (boolTrueOne & boolFalseOne)
+            //{
+            //    Console.WriteLine("boolTrueOne & boolFalseOne: " + boolTrueOne + boolFalseOne);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("boolTrueOne & boolFalseOne: " + boolTrueOne + boolFalseOne);
+            //}
+
+            //if (boolTrueOne || boolTrueTwo)
+            //{
+            //    //Console.WriteLine("boolTrueOne || bootTrueTwo: " + boolTrueOne + boolTrueTwo);
+            //}
+        }
+    }
+}
