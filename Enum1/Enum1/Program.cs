2c069c775f7ad49009ad438942de7e69a11aaf42
WebsterSweden <webster@webster.se>
2020-10-11 16:46:44 +0200

    Edit Enum1.

--- Enum1/Enum1/Program.cs
+++ Enum1/Enum1/Program.cs
@@ -4,8 +4,25 @@ namespace Enum1
 {
     class Program
     {
+        enum EnumOne
+        {
+            First,
+            Second,
+            Third
+        }
+
+        enum EnumTwo
+        {
+            Fourth = 4,
+            Fifth = 5,
+            Sixth = 6
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(EnumOne.First);
+
+            Console.WriteLine(EnumTwo.Fifth);
         }
     }
 }
