74fef50d3a518b55842de647d457df5ae100c0fd
WebsterSweden <webster@webster.se>
2020-10-13 21:34:39 +0200

    Edit Class2.

---
+++ Class2/Class2/ClassOne.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class2
+{
+    class ClassOne
+    {
+        public string classOneStringOne;
+        public string classOneStringTwo;
+        public int classOneIntOne;
+
+        public static string classOneStringStaticOne = "";
+    }
+}
--- Class2/Class2/Program.cs
+++ Class2/Class2/Program.cs
@@ -6,6 +6,26 @@ namespace Class2
     {
         static void Main(string[] args)
         {
+            ClassOne classOneObjectOne = new ClassOne();
+            string classOneStringOneVariable = classOneObjectOne.classOneStringOne;
+            Console.WriteLine("classOneStringOneVariable: " + classOneStringOneVariable);
+
+            ClassOne classOneObjectTwo = new ClassOne();
+            classOneObjectTwo.classOneStringOne = "classOneObjectTwo.classOneStringOneValue";
+            Console.WriteLine(classOneObjectTwo.classOneStringOne);
+            classOneObjectTwo.classOneStringTwo = "classOneObjectTwo.classOneStringTwoValue";
+            Console.WriteLine(classOneObjectTwo.classOneStringTwo);
+            classOneObjectTwo.classOneIntOne = 3;
+            Console.WriteLine(classOneObjectTwo.classOneIntOne);
+
+            string classOneStringStaticOne = ClassOne.classOneStringStaticOne;
+            Console.WriteLine("classOneStringStaticOne: " + classOneStringStaticOne);
+            classOneStringStaticOne = "classOneStringStaticOneValue";
+            Console.WriteLine("classOneStringStaticOne: " + classOneStringStaticOne);
+
+            string classOneStringStaticTwo = ClassOne.classOneStringStaticOne;
+            classOneStringStaticTwo = "classOneStringStaticTwoValue";
+            Console.WriteLine("classOneStringStaticTwo: " + classOneStringStaticTwo);
         }
     }
 }
