4f54e600e307206239ec2643a6a634501f79a964
WebsterSweden <webster@webster.se>
2020-10-19 20:28:11 +0200

    Edit ParsTryeParse1.

--- ParseTryParse1/ParseTryParse1/Program.cs
+++ ParseTryParse1/ParseTryParse1/Program.cs
@@ -6,6 +6,31 @@ namespace ParseTryParse1
     {
         static void Main(string[] args)
         {
+            string stringOne = "ABC";
+            string stringTwo = "123";
+            string stringThree = "0.03";
+            string stringFour = "";
+
+            try
+            {
+                //int.Parse(stringOne);
+                //Console.WriteLine(stringOne);
+
+                //int.Parse(stringTwo);
+                //Console.WriteLine(stringTwo);
+
+                //int.Parse(stringThree);
+                //Console.WriteLine(stringThree);
+
+                int.Parse(stringFour);
+                Console.WriteLine(stringFour);
+            }
+            catch (Exception exceptionMessage)
+            {
+                Console.WriteLine(exceptionMessage.Message);
+            };
+
+
         }
     }
 }
