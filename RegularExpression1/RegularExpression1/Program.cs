4ac9a4b402df88032dd205ec4d92505abfa10d51
WebsterSweden <webster@webster.se>
2020-10-20 16:59:57 +0200

    Edit RegularExpression1.

--- RegularExpression1/RegularExpression1/Program.cs
+++ RegularExpression1/RegularExpression1/Program.cs
@@ -8,12 +8,23 @@ namespace RegularExpression1
         static void Main(string[] args)
         {
             Regex regexOne = new Regex(@"\d+");
-            Match matchRegexOne = regexOne.Match("ABC123DEF");
+            Match matchRegexOne = regexOne.Match("ABC123DEF456");
 
             if (matchRegexOne.Success)
             {
                 Console.WriteLine("matchRegexOne.Value: " + matchRegexOne.Value);
             }
+
+            string regexTwoInput = "ABC123DEF456";
+
+            Regex regexTwo = new Regex(@"[0-9]+");
+            Match matchRegexTwo = regexTwo.Match(regexTwoInput);
+            Console.WriteLine(matchRegexTwo);
+
+            var regexECMA = new Regex(@"\d+", RegexOptions.ECMAScript);
+            Console.WriteLine(regexECMA);
+            Match matchRegexECMA = regexECMA.Match("abc123ABC456");
+            Console.WriteLine(matchRegexECMA);
         }
     }
 }
