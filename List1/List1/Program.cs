b590608d8ec03f0b08120bfb4dde10a1e43b5b47
WebsterSweden <webster@webster.se>
2020-10-22 23:35:04 +0200

    Edit List1.

--- List1/List1/Program.cs
+++ List1/List1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace List1
 {
@@ -6,6 +7,42 @@ namespace List1
     {
         static void Main(string[] args)
         {
+            List<int> primeNumbers = new List<int>();
+            //Console.WriteLine(primeNumbers);
+            primeNumbers.Add(1);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+
+            foreach (int primeNumber in primeNumbers)
+            {
+                Console.WriteLine(primeNumber);
+            }
+
+            var cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Mumbai");
+            cities.Add("Chicago");
+            cities.Add(null);
+
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            var bigCities = new List<string>()
+            {
+                "New York",
+                "London",
+                "Mumbai",
+                "Chicago"
+            };
+
+            foreach (var bigCity in bigCities)
+            {
+                Console.WriteLine(bigCity);
+            }
         }
     }
 }
