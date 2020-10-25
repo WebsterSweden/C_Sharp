34b861e974f8398c9c814e74844b211b951f9f54
WebsterSweden <webster@webster.se>
2020-10-25 22:43:42 +0100

    Edit whileStatement1.

--- whileStatement1/whileStatement1/Program.cs
+++ whileStatement1/whileStatement1/Program.cs
@@ -7,14 +7,30 @@ namespace whileStatement1
     {
         static void Main(string[] args)
         {
-            int whileIndexOne = 0;
+            //int whileIndexOne = 0;
 
-            while (whileIndexOne <= 6)
+            //while (whileIndexOne <= 6)
+            //{
+            //    Console.WriteLine("whileIndexOne: " + whileIndexOne);
+            //    Thread.Sleep(500);
+            //    whileIndexOne++;
+            //}
+
+            string whileStringTwo = "whileStringTwoBeforeWhileStatement";
+            //Console.WriteLine("whileStringTwo: " + whileStringTwo);
+            int whileIndexTwo = 0;
+            Console.WriteLine("whileIndexTwo before while statement: " + whileIndexTwo);
+            while (whileIndexTwo < 3)
             {
-                Console.WriteLine("whileIndexOne: " + whileIndexOne);
-                Thread.Sleep(500);
-                whileIndexOne++;
+                whileStringTwo = "whileStringTwoInsideWhileStatement";
+                //Console.WriteLine("whileStringTwo: " + whileStringTwo);
+                Console.WriteLine("whileIndexTwo inside while statement: " + whileIndexTwo);
+                Thread.Sleep(1000);
+                whileIndexTwo++;
             }
+            whileStringTwo = "whileStringTwoAfterWhileStatement";
+            //Console.WriteLine("whileStringTwo: " + whileStringTwo);
+            Console.WriteLine("whileIndexTwo after while statement: " + whileIndexTwo);
         }
     }
 }
