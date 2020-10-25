2007f20c9557c89e0805cfb3094d5091827c5a9d
WebsterSweden <webster@webster.se>
2020-10-25 13:06:53 +0100

    Edit List1.

--- List1/List1/Program.cs
+++ List1/List1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace List1
@@ -47,6 +48,44 @@ namespace List1
             List<string> listOnes = new List<string>() { "stringOne", "stringTwo", "stringThree" };
             foreach (string listOne in listOnes) { Console.WriteLine(listOne); }
             Console.WriteLine(listOnes.Count);
+
+            Console.WriteLine("----------");
+
+            List<string> listStringOnes = new List<string>();
+            listStringOnes.Insert(0, "Zero");
+            listStringOnes.Insert(1, "One");
+            listStringOnes.Insert(2, "Two");
+
+            foreach (string listStringOne in listStringOnes)
+            {
+                Console.WriteLine("" + listStringOne);
+            }
+
+            for (int i = 0; i < listStringOnes.Count; i++)
+            {
+                Console.WriteLine(listStringOnes[i]);
+            }
+
+            List<object> listObjectOnes = new List<object>();
+            listObjectOnes.Add("String");
+            listObjectOnes.Add(3);
+            listObjectOnes.Add(0.9);
+
+            foreach (object listObjectOne in listObjectOnes)
+            {
+                Console.WriteLine(listObjectOne);
+            }
+
+            ArrayList arrayListOnes = new ArrayList();
+            arrayListOnes.Add("String");
+            arrayListOnes.Add(3);
+            arrayListOnes.Add(0.9);
+
+            foreach (var arrayListOne in arrayListOnes)
+            {
+                Console.WriteLine(arrayListOne);
+            }
+
         }
     }
 }
