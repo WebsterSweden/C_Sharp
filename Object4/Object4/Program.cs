2c33c57d4de77bf77b53ccb6d8ad37fd74d66118
WebsterSweden <webster@webster.se>
2020-11-03 23:44:58 +0100

    Edit Object4.

--- Object4/Object4/Program.cs
+++ Object4/Object4/Program.cs
@@ -6,6 +6,38 @@ namespace Object4
     {
         static void Main(string[] args)
         {
+            Character player = new Character();
+            player.CharacterName = "Tie Fighter";
+            player.Strength = 12;
+            Console.WriteLine($"{player.CharacterName} {player.Strength}");
+
+            Character opponent = new Character();
+            opponent.CharacterName = "Stormtrooper";
+            opponent.Strength = 9;
+            Console.WriteLine($"{opponent.CharacterName} {opponent.Strength}");
+
+            Battle battleParticipants = new Battle();
+            battleParticipants.BattleStart(player, opponent);
+            Console.WriteLine($"{battleParticipants.PlayerObject.CharacterName} {battleParticipants.PlayerObject.Strength}");
+            Console.WriteLine($"{battleParticipants.OpponentObject.CharacterName} {battleParticipants.OpponentObject.Strength}");
+        }
+    }
+
+    class Character
+    {
+        public string CharacterName { get; set; }
+
+        public int Strength { get; set; }
+    }
+    class Battle
+    {
+        public Character PlayerObject;
+        public Character OpponentObject;
+
+        public void BattleStart(Character playerParameter, Character opponentParameter)
+        {
+            PlayerObject = playerParameter;
+            OpponentObject = opponentParameter;
         }
     }
 }
