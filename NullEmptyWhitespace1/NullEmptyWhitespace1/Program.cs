0b539c19f455ab5d1cf57f798354b9a346c9438a
WebsterSweden <webster@webster.se>
2020-11-06 23:55:54 +0100

    Edit NullEmptyWhitespace1.

--- NullEmptyWhitespace1/NullEmptyWhitespace1/Program.cs
+++ NullEmptyWhitespace1/NullEmptyWhitespace1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace NullEmptyWhitespace1
 {
@@ -6,6 +7,43 @@ namespace NullEmptyWhitespace1
     {
         static void Main(string[] args)
         {
+            string nullEmptyWhitespace;
+
+            nullEmptyWhitespace = null;
+
+            if (String.IsNullOrWhiteSpace(nullEmptyWhitespace))
+            //if (nullEmptyWhitespace == null)
+            {
+                Console.WriteLine("nullEmptyWhitespace is null");
+            }
+            else
+            {
+                Console.WriteLine("nullEmptyWhitespace is not null");
+            }
+
+            nullEmptyWhitespace = "";
+
+            if (String.IsNullOrWhiteSpace(nullEmptyWhitespace))
+            //if (nullEmptyWhitespace == "")
+            {
+                Console.WriteLine("nullEmptyWhitespace is empty");
+            }
+            else
+            {
+                Console.WriteLine("nullEmptyWhitespace is not empty");
+            }
+
+            nullEmptyWhitespace = " ";
+
+            if (String.IsNullOrWhiteSpace(nullEmptyWhitespace))
+            //if (nullEmptyWhitespace == " ")
+            {
+                Console.WriteLine("nullEmptyWhitespace have whitespace");
+            }
+            else
+            {
+                Console.WriteLine("nullEmptyWhitespace have not whitespace");
+            }
         }
     }
 }
