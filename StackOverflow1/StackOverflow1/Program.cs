a33d60538a5617fa10fefe3d597088bae9a2d075
WebsterSweden <webster@webster.se>
2020-11-08 00:19:58 +0100

    Edit StackOverflow1.

--- StackOverflow1/StackOverflow1/Program.cs
+++ StackOverflow1/StackOverflow1/Program.cs
@@ -6,6 +6,16 @@ namespace StackOverflow1
     {
         static void Main(string[] args)
         {
+            // TODO: How to catch the Stack overflow exception?
+            // Expected output: Stack overflow.
+            Program recursive = new Program();
+            try { recursive.RecursiveMethod(); }
+            catch (Exception exceptionMessage) { Console.WriteLine(exceptionMessage.Message); }
+        }
+
+        void RecursiveMethod()
+        {
+            RecursiveMethod();
         }
     }
 }
