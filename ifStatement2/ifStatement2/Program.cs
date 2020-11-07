659bcac7802ff61fc3962fda9529d80fb5bbe44a
WebsterSweden <webster@webster.se>
2020-11-07 14:26:37 +0100

    Edit ifStatement2.

--- ifStatement2/ifStatement2/Program.cs
+++ ifStatement2/ifStatement2/Program.cs
@@ -6,6 +6,22 @@ namespace ifStatement2
     {
         static void Main(string[] args)
         {
+            int integerOne = 3;
+            int integerTwo = 6;
+            int integerThree = 9;
+
+            bool boolTrue = true;
+            bool boolFalse = false;
+
+            if (boolTrue) { Console.WriteLine("if statement 1 with condition true"); }
+            if (boolTrue) { Console.WriteLine("if statement 2 with condition true"); }
+            if (boolTrue) { Console.WriteLine("if statement 3 with condition true"); }
+
+            Console.WriteLine();
+
+            if (boolTrue) { Console.WriteLine("if statement 1 with condition true"); }
+            else if (boolTrue) { Console.WriteLine("else if 1 statement with condition true"); }
+            else if (boolTrue) { Console.WriteLine("else if 2 statement with condition true"); }
         }
     }
 }
