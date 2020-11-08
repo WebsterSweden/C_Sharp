c15ede6bbad48a31db10f1d09a1afe781f6588d1
WebsterSweden <webster@webster.se>
2020-11-09 00:49:52 +0100

    Edit Color1.

--- Color1/Color1/Program.cs
+++ Color1/Color1/Program.cs
@@ -6,6 +6,18 @@ namespace Color1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Console.ForegroundColor: {Console.ForegroundColor}");
+            Console.WriteLine($"Console.BackgroundColor: {Console.BackgroundColor}");
+
+            //Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.Clear();
+
+            Console.WriteLine($"Console.ForegroundColor: {Console.ForegroundColor}");
+            Console.WriteLine($"Console.BackgroundColor: {Console.BackgroundColor}");
         }
     }
 }
