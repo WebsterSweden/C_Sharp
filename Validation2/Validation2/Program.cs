87b596aee542b3bbfa3ece368961f54318e47a28
WebsterSweden <webster@webster.se>
2020-11-09 23:03:40 +0100

    Edit Validation2.

--- Validation2/Validation2/Program.cs
+++ Validation2/Validation2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Validation2
 {
@@ -6,6 +7,32 @@ namespace Validation2
     {
         static void Main(string[] args)
         {
+            // Check if string is null.
+            string nullString = null;
+
+            if (nullString == null)
+            {
+                Console.WriteLine($"nullString has a value of: {nullString}: ");
+                //Console.WriteLine($"nullString.Length{nullString.Length}: ");
+            }
+
+            Console.WriteLine("   ".IsEmptyOrAllSpaces());
+            Console.WriteLine("".IsEmptyOrAllSpaces());
+            Console.WriteLine(" ".IsEmptyOrAllSpaces());
+            Console.WriteLine("ABC   ".IsEmptyOrAllSpaces());
+            Console.WriteLine("   DEF".IsEmptyOrAllSpaces());
+            Console.WriteLine("ABC   DEF".IsEmptyOrAllSpaces());
+        }
+    }
+
+    // Class non-generic, static, non-nested.
+    // Every method static.
+    // First parameter should us the this keyword.
+    static class LinqHelper
+    {
+        public static bool IsEmptyOrAllSpaces(this string stringToCheck)
+        {
+            return null != stringToCheck && stringToCheck.All(c => c.Equals(' '));
         }
     }
 }
