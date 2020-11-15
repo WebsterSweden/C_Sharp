16c65e8dcce2ef93224579c87c732eff227d5dda
WebsterSweden <webster@webster.se>
2020-11-15 03:35:08 +0100

    Edit Number1.

--- Number1/Number1/Program.cs
+++ Number1/Number1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Number1
 {
@@ -6,6 +7,35 @@ namespace Number1
     {
         static void Main(string[] args)
         {
+            //double[] numberArray = { -3.0, -1.5, 0.0, 1.5, 3.0 };
+            double[] numberArray = { 0.0, 1.5, 3.0, 6.0, 12.0 };
+
+            Console.WriteLine(numberArray.Min());
+            Console.WriteLine(numberArray.Max());
+
+            Console.WriteLine(Double.MinValue);
+            Console.WriteLine(double.MinValue);
+
+            Console.WriteLine(Double.MaxValue);
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine(Math.Min(3, 6));
+            Console.WriteLine(Math.Max(3, 6));
+
+            Console.WriteLine("----------");
+
+            double result = 0.0;
+
+            foreach (var number in numberArray)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+
+            double averageNumber = result / numberArray.Length;
+
+            Console.WriteLine(averageNumber);
         }
     }
 }
