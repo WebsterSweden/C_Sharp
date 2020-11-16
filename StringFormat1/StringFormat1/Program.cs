cc2983a04e276d5c9e73c551e3bd1f83ad457fbd
WebsterSweden <webster@webster.se>
2020-11-17 00:33:51 +0100

    StringFormat1.

--- StringFormat1/StringFormat1/Program.cs
+++ StringFormat1/StringFormat1/Program.cs
@@ -6,6 +6,12 @@ namespace StringFormat1
     {
         static void Main(string[] args)
         {
+            int intOne = 9;
+
+            // String format for int.
+            Console.WriteLine(string.Format("{0}", intOne));
+            Console.WriteLine(string.Format("{0:00}", intOne));
+            Console.WriteLine(string.Format("{0:000000}", intOne));
         }
     }
 }
