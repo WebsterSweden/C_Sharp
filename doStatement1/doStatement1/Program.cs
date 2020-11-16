94d22b98eb23596d488e7758781faa5bb468e948
WebsterSweden <webster@webster.se>
2020-11-17 00:35:47 +0100

    doStatement1.

--- doStatement1/doStatement1/Program.cs
+++ doStatement1/doStatement1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace doStatement1
 {
@@ -6,13 +7,62 @@ namespace doStatement1
     {
         static void Main(string[] args)
         {
-            int doWhileIndexOne = 7;
+            //int doWhileIndexOne = 0;
 
-            do
+            //do
+            //{
+            //    Console.WriteLine("doWhileIndexOne: " + doWhileIndexOne);
+            //    doWhileIndexOne++;
+            //}
+            //while (doWhileIndexOne <= 6);
+
+            //do
+            //{
+            //    while (!Console.KeyAvailable)
+            //    {
+            //        Console.WriteLine("Press the ESC to terminate the application");
+            //        //Thread.Sleep(500);
+            //        string input = Console.ReadLine();
+            //        Console.WriteLine(input);
+            //    }
+            //}
+            //while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            //do
+            //{
+            //    Console.WriteLine("do statement outer");
+            //    Thread.Sleep(500);
+            //}
+            //while (true);
+
+            //do
+            //{
+            //Console.WriteLine("do statement outer while not press escape key");
+            //Thread.Sleep(500);
+            //while (!Console.KeyAvailable)
+            //{
+            //Console.WriteLine("Console.KeyAvailable");
+            //Console.ReadKey();
+            //string input = Console.ReadLine();
+            //Console.WriteLine(input);
+            //}
+            //}
+            //while (Console.ReadKey().Key == ConsoleKey.Escape);
+            //while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            //while (Console.ReadKey(false).Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Press ESC to stop: ");
+
+            while ((!Console.KeyAvailable) && Console.ReadKey(true).Key != ConsoleKey.Escape)
             {
-                Console.WriteLine("doWhileIndexOne: " + doWhileIndexOne);
+                //Console.Write($"Press ESC to terminate the application: ");
+                //string input = Console.ReadLine();
+                //Console.WriteLine($"Entered input: {Console.ReadKey().Key}. The application continue to execute");
+                Console.WriteLine("The Enter key is not pressed");
+                Console.Write("Enter a string: ");
+                string input = Console.ReadLine();
+                Console.WriteLine(input);
             }
-            while (doWhileIndexOne <= 6);
         }
     }
 }
