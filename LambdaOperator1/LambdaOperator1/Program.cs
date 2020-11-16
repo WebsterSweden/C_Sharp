a70a9183071f779eb1fcb643abc1b03c5eed8158
WebsterSweden <webster@webster.se>
2020-11-17 00:32:01 +0100

    Edit LambdaOperator1.

--- LambdaOperator1/LambdaOperator1/Program.cs
+++ LambdaOperator1/LambdaOperator1/Program.cs
@@ -6,6 +6,20 @@ namespace LambdaOperator1
     {
         static void Main(string[] args)
         {
+            int result;
+
+            Func<int, int> multiplyByFive = num => num * 5;
+            result = multiplyByFive(7);
+
+            Func<int, int> multiplyByFiveReturnValue = num =>
+            {
+                int product = num * 5;
+                return product;
+            };
+            result = multiplyByFive(7);
+
+            Func<int, int, int> multiplyTwoNumbers = (a, b) => a * b;
+            result = multiplyTwoNumbers(5, 7);
         }
     }
 }
