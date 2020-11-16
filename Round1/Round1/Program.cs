8cb8dd67a8f6bd12aed0dbcc361ca08c1238d5b4
WebsterSweden <webster@webster.se>
2020-11-17 00:32:44 +0100

    Edit Round1.

--- Round1/Round1/Program.cs
+++ Round1/Round1/Program.cs
@@ -6,6 +6,19 @@ namespace Round1
     {
         static void Main(string[] args)
         {
+            double doubleOne = 0.123456789;
+
+            Console.WriteLine(doubleOne);
+
+            Console.WriteLine(Math.Round(doubleOne));
+
+            Console.WriteLine(Math.Round(doubleOne, 0));
+
+            Console.WriteLine(Math.Round(doubleOne, 1));
+
+            Console.WriteLine(Math.Round(doubleOne, 2));
+
+            Console.WriteLine(Math.Round(doubleOne, 3));
         }
     }
 }
