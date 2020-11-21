fcffbab556d5ab90c4f96a2155a5e03e59b4f23a
WebsterSweden <webster@webster.se>
2020-11-21 12:54:13 +0100

    Edit Remainder1.

--- Remainder1/Remainder1/Denomination.cs
+++ Remainder1/Remainder1/Denomination.cs
@@ -38,7 +38,7 @@ namespace Remainder1
         {
             decimal denominationAmount = Math.Round(denominationParameter);
             decimal operand;
-            decimal reminder;
+            decimal remainder;
 
             decimal thousandBill;
             decimal fiveHundredBill;
@@ -54,74 +54,74 @@ namespace Remainder1
             Console.WriteLine($"denominationAmount: {denominationAmount}");
 
             // Calculate how many 1000 bills.
-            reminder = denominationAmount % 1000;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 1000;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             thousandBill = Math.Round(operand, 0) / 1000;
-            Console.WriteLine($"denominationAmount: {denominationAmount} thousandBill: {thousandBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} thousandBill: {thousandBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 500 bills.
-            reminder = denominationAmount % 500;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 500;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             fiveHundredBill = Math.Round(operand, 0) / 500;
-            Console.WriteLine($"denominationAmount: {denominationAmount} fiveHundredBill: {fiveHundredBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} fiveHundredBill: {fiveHundredBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 200 bills.
-            reminder = denominationAmount % 200;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 200;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             twoHundredBill = Math.Round(operand, 0) / 200;
-            Console.WriteLine($"denominationAmount: {denominationAmount} twoHundredBill: {twoHundredBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} twoHundredBill: {twoHundredBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 100 bills.
-            reminder = denominationAmount % 100;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 100;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             hundredBill = Math.Round(operand, 0) / 100;
-            Console.WriteLine($"denominationAmount: {denominationAmount} hundredBill: {hundredBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} hundredBill: {hundredBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 50 bills.
-            reminder = denominationAmount % 50;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 50;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             fiftyBill = Math.Round(operand, 0) / 50;
-            Console.WriteLine($"denominationAmount: {denominationAmount} fiftyBill: {fiftyBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} fiftyBill: {fiftyBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 20 bills.
-            reminder = denominationAmount % 20;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 20;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             twentyBill = Math.Round(operand, 0) / 20;
-            Console.WriteLine($"denominationAmount: {denominationAmount} TwentyBill: {TwentyBill} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} twentyBill: {twentyBill} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 10 coins.
-            reminder = denominationAmount % 10;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 10;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             tenCoin = Math.Round(operand, 0) / 10;
-            Console.WriteLine($"denominationAmount: {denominationAmount} tenCoin: {tenCoin} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} tenCoin: {tenCoin} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 5 coins.
-            reminder = denominationAmount % 5;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 5;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             fiveCoin = Math.Round(operand, 0) / 5;
-            Console.WriteLine($"denominationAmount: {denominationAmount} fiveCoin: {fiveCoin} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} fiveCoin: {fiveCoin} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 2 coins.
-            reminder = denominationAmount % 2;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 2;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             twoCoin = Math.Round(operand, 0) / 2;
-            Console.WriteLine($"denominationAmount: {denominationAmount} twoCoin: {twoCoin} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} twoCoin: {twoCoin} operand: {operand} reminder: {remainder}");
 
             // Calculate how many 1 coins.
-            reminder = denominationAmount % 1;
-            operand = denominationAmount - reminder;
-            denominationAmount = reminder;
+            remainder = denominationAmount % 1;
+            operand = denominationAmount - remainder;
+            denominationAmount = remainder;
             oneCoin = Math.Round(operand, 0) / 1;
-            Console.WriteLine($"denominationAmount: {denominationAmount} oneCoin: {oneCoin} operand: {operand} reminder: {reminder}");
+            Console.WriteLine($"denominationAmount: {denominationAmount} oneCoin: {oneCoin} operand: {operand} reminder: {remainder}");
         }
     }
 }
