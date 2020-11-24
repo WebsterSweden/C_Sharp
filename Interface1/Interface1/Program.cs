80d4e73bd146c1be09553641f056e676a6f638b0
WebsterSweden <webster@webster.se>
2020-11-24 21:31:14 +0100

    Edit Interface1.

--- Interface1/Interface1/Program.cs
+++ Interface1/Interface1/Program.cs
@@ -2,10 +2,40 @@ using System;
 
 namespace Interface1
 {
+    interface IinterfaceOne
+    {
+        //string stringFieldOne = "stringFieldOne";
+
+        //public string stringPropertyOne { get; set; } = "stringPropertyOne";
+        //public string stringPropertyOne { get; set; }
+
+        //IinterfaceOne() { }
+
+        //void MethodOne()
+        //{
+        //    Console.WriteLine("MethodOne");
+        //}
+
+        void MethodNoBody();
+    }
+
+    class ClassOne : IinterfaceOne
+    {
+        public void MethodNoBody()
+        {
+            Console.WriteLine("ClassOne : IinterfaceOne");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            //interface IinterfaceOne { }
+
+            ClassOne classOne = new ClassOne();
+
+            classOne.MethodNoBody();
         }
     }
 }
