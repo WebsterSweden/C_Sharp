bbaec3323fb32202b6043462300eda688500b54d
WebsterSweden <webster@webster.se>
2020-11-25 20:09:10 +0100

    Edit using1.

--- using1/using1/Program.cs
+++ using1/using1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace using1
 {
@@ -6,6 +7,29 @@ namespace using1
     {
         static void Main(string[] args)
         {
+            string path = @"C:\using1.txt";
+
+            if (!File.Exists(path))
+            {
+                // Create text file.
+                using (StreamWriter streamWriter = File.CreateText(path))
+                {
+                    streamWriter.WriteLine("First");
+                    streamWriter.WriteLine("Second");
+                    streamWriter.WriteLine("Third");
+                }
+            }
+
+            // Open text file and read the content.
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string stringLine;
+
+                while ((stringLine = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(stringLine);
+                }
+            }
         }
     }
 }
