2afc008f35736f3661428ee16593fc25f3872cf9
WebsterSweden <webster@webster.se>
2020-12-02 18:13:49 +0100

    Edit SolutionProject2.

--- SolutionProject2/NewProjectOne/Program.cs
+++ SolutionProject2/NewProjectOne/Program.cs
@@ -1,12 +1,12 @@
 using System;
 
-namespace SolutionProject2
+namespace NewProjectOne
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SolutionProject2");
+            Console.WriteLine("NewProjectOne");
         }
     }
 }
